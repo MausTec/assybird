2b88a3135ffb940e6ee3c3d62373adad4a630895
Mau Abata <mauabata@gmail.com>
2021-02-09 00:19:03 -0600

    Documentation. Never 'eard of 'er.

--- src/Game/index.js
+++ src/Game/index.js
@@ -7,30 +7,122 @@ import backgroundImage from '../assets/assy-background.png'
 import VibratorSearchButton from "../Buttplug/VibratorSearchButton";
 import VibratorControls from "../Buttplug/VibratorControls";
 
+/**
+ * This is Flappy Bird.
+ * In React.
+ * But played with your ass.
+ *
+ * What more do you expect?
+ */
 class Game extends Component {
+    /**
+     * Primary control comes from the Edge-o-Matic. This context provides raw data.
+     */
     static contextType = ReadingsContext;
+
+    /**
+     * Game FPS. Increase for sadness.
+     * @type {number}
+     */
     FPS = 30;
+
+    /**
+     * The rightmost position of the bird on screen, in percent.
+     * @type {number}
+     */
     BirdRight = 15;
+
+    /**
+     * Calculations ramp up to "max" over this many points. Anything higher than this is a limit break.
+     * (I don't know what'll happen.)
+     * @type {number}
+     */
     MaxScore = 20;
+
+    /**
+     * This is the rate at which your bird falls. Also known as "gravity". Your bird drops (1-this)% per frame.
+     * @type {number}
+     */
     DecayCoefficient = 0.98;
+
+    /**
+     * This is how high your bird flaps, in percent, per flappy flap.
+     * @type {number}
+     */
     FlapAmount = 20;
+
+    /**
+     * Increasing this adds some wiggle room around pipes. This game was meant to be played with your anus, which is
+     * arguably not a very accurate game controller. That's probably why most game systems use your fingers instead.
+     * @type {number}
+     */
     PipeCollisionThreshold = 1;
 
     constructor(props) {
         super(props);
 
         this.defaultState = {
+            /**
+             * Signal that a game has begun.
+             */
             started: false,
+
+            /**
+             * Signal that the bird somehow crashed. Or we won?
+             */
             gameOver: false,
+
+            /**
+             * This signals that the bird crashed, specifically. It's mostly to load the right
+             * game over graphics.
+             */
             crashed: false,
+
+            /**
+             * This is almost deprecated, but we don't start the game until you flap for the first time.
+             */
             birdCaptured: false,
+
+            /**
+             * This is how many jolly walls of anal beads you have launched your poor flapping buttplug through.
+             */
             score: 0,
+
+            /**
+             * This goes from 1 to 2 and above, and is a speed multiplier.
+             */
             speed: 1,
+
+            /**
+             * This is just a timestamp in the game. It's how many frames have elapsed since it started.
+             */
             ticks: 0,
+
+            /**
+             * Height of the bird, in percent. I think this is the bottom of the bird, yeah.
+             */
             birdHeight: 0,
+
+            /**
+             * Bird Height and Virtual Arousal are related. This is almost deprecated, since arousal was the first
+             * metric from the Edgeo-o-Matic that we used for bird altitude.
+             */
             virtualArousal: 50,
+
+            /**
+             * @ignore
+             */
             konami: false,
+
+            /**
+             * @ignore
+             */
             konamiIndex: 0,
+
+            /**
+             * List of pipes. They're not really pipes, they're bright pink anal beads. But then again, it's not really
+             * a bird, now is it?
+             */
             pipes: []
         }
 
@@ -46,23 +138,36 @@ class Game extends Component {
         this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
+    /**
+     * Hack to focus the playfield so our spacebar can space, bar.
+     */
     componentDidMount() {
         if (this.playfield) {
             this.playfield.focus();
         }
     }
 
+    /**
+     * This starts a new game. It resets the state, and begins the loop timer.
+     */
     startGame() {
         this.setState({...this.defaultState, started: true});
         this.lastPipeRender = 0;
         this.tickInterval = setInterval(this.tick, 1000 / this.FPS);
     }
 
+    /**
+     * This ends the game. Useful if your buttplug slammed into a wall.
+     * @param addtlState
+     */
     endGame(addtlState = {}) {
         this.setState({gameOver: true, ...addtlState});
         clearInterval(this.tickInterval);
     }
 
+    /**
+     * Adds a fresh new set of anal beads to guide your little adventurer through.
+     */
     addPipe() {
         const {speed, ticks} = this.state;
         const gap = (this.MaxScore - speed) + Math.floor(Math.random() * ((this.MaxScore / 2) - speed));
@@ -80,12 +185,19 @@ class Game extends Component {
         })
     }
 
+    /**
+     * Removes pipes which are no longer on screen.
+     */
     removePipes() {
         this.setState({
             pipes: this.state.pipes.filter(p => p.position > 0)
         });
     }
 
+    /**
+     * Moves pipes [actually anal beads] over by a set amount.
+     * @param speed
+     */
     shiftPipes(speed) {
         let pipes = [...this.state.pipes];
 
@@ -99,6 +211,12 @@ class Game extends Component {
         })
     }
 
+    /**
+     * This calculates the height of the bird based on arousal, and whether or not we have indeed captured the bird.
+     * It's not accurate to what is on screen, since the CSS animation smoothing stuff.
+     * @see calculateActualBirdHeight
+     * @returns {number}
+     */
     calculateBirdHeight() {
         const {arousalLimit, offlineMode} = this.props
         let height;
@@ -123,6 +241,11 @@ class Game extends Component {
         return height;
     }
 
+    /**
+     * This calculates the actual height of the bird, in percent. This is necessary to adjust for any CSS animations
+     * that happen, since the bird movement is smoothed by CSS and not JavaScript. Cool, huh?
+     * @returns {number}
+     */
     calculateActualBirdHeight() {
         if (!this.playfield || !this.birdRef) {
             return 50;
@@ -135,6 +258,12 @@ class Game extends Component {
         return Math.floor(((playField.height - birdMid) / playField.height) * 100)
     }
 
+    /**
+     * All of this determines if your buttplug crashed into anal beads.
+     * There's some math here, because I chose to store all the values in percents so I have to translate it back to
+     * window size. This also means you can play flappy bird with your asshole while an asshole resizes the window
+     * constantly.
+     */
     calculateCollision() {
         const height = this.calculateActualBirdHeight();
         const playfieldHeight = (this.playfield && this.playfield.getBoundingClientRect().height) || 100;
@@ -170,6 +299,10 @@ class Game extends Component {
         })
     }
 
+    /**
+     * This is the main game loop, which adds pipes, shifts pipes, calculates collision with pipes.
+     * You know. Game loop stuff.
+     */
     tick() {
         const ticks = this.state.ticks + 1;
         let virtualArousal = this.state.virtualArousal;
@@ -194,11 +327,19 @@ class Game extends Component {
         this.setState({ticks, virtualArousal});
     }
 
+    /**
+     * Event bind to pass to startGame
+     * @param e
+     */
     handleStartClick(e) {
         e.preventDefault();
         this.startGame();
     }
 
+    /**
+     * Here there be flappin'
+     * @param e
+     */
     handleKeyDown(e) {
         const konami = [
             38, 38,
